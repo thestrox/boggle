f5bcac2bc5fa105bef994d6da8ea03a0a5097e33
Sabal Thapaliya <thestrox.1@gmail.com>
2020-03-07 11:44:47 +0545

    Refactor root/lib utilities to app/lib

---
+++ app/lib/boggle_generator.rb
@@ -0,0 +1,16 @@
+class BoggleGenerator
+  include BoggleConstants
+
+    # Returns the randomly generated board of given board_size * board_size
+    def self.get_randomize_board(board_size)
+      # Shuffle the DICE
+      DICES.shuffle
+      dice_faces = []
+  
+      DICES.each do |dice|
+        dice_faces.push(dice.sample)
+      end
+  
+      return dice_faces.each_slice(board_size).to_a
+    end
+end
--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,5 @@ module Boggle
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.eager_load_paths << Rails.root.join('lib/boggle')
   end
 end
